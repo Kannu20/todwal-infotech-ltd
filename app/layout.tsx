a3f9b44e1c32950fdd69e8f850d7dced1a0cea5b
Kannu20 <kannutodwal@gmail.com>
2026-02-20 12:09:45 +0530

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,10 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Todwal Infotech - High-Converting Websites for Local Businesses',
   description: 'Professional web development for clinics, gyms, salons, and coaching centers',
+
+   verification: {
+    google: 'VxWhXAFb2QOijzduAwIKcKt71hsfi2WV_6gf5rgMnrg',
+  },
 }
 
 export default function RootLayout({
